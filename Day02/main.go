ebadf21e9f8d86778d0fb3bf749cb8becc5a30db
Cyril HERRERA <cherrera@com-company.fr>
2025-12-03 13:27:08 +0100

    Done Day2, Part2

--- Day02/main.go
+++ Day02/main.go
@@ -2,6 +2,7 @@ package Day02
 
 import (
 	"adventOfCode/utils"
+	"bytes"
 	"fmt"
 	"math"
 	"strconv"
@@ -53,9 +54,59 @@ func ResolvePart1(data []string) int {
 
 	return count
 }
+
 func ResolvePart2(data []string) int {
-	return 0
+	count := 0
+	idRanges := utils.RegSplit(data[0], ",")
+
+	for _, idRange := range idRanges {
+		bornes := utils.RegSplit(idRange, "-")
+
+		startId := bornes[0]
+		endId := bornes[1]
+
+		currentId, err := strconv.Atoi(startId)
+		finishId, err := strconv.Atoi(endId)
+
+		if err != nil {
+			panic("aaaah")
+		}
+
+		for {
+			currentIdStr := fmt.Sprintf("%v", currentId)
+
+			var patterns []string
+			for patternLength := 1; patternLength <= len(currentIdStr)/2; patternLength++ {
+				currentPattern := currentIdStr[:patternLength]
+				patterns = append(patterns, currentPattern)
+
+				if patternLength%len(currentIdStr) != patternLength {
+					continue
+				}
+
+				var buffer bytes.Buffer
+
+				for i := 0; i < (len(currentIdStr) / patternLength); i++ {
+					buffer.WriteString(currentPattern)
+				}
+
+				if buffer.String() == currentIdStr {
+					count += currentId
+					break
+				}
+			}
+
+			if currentId == finishId {
+				break
+			}
+
+			currentId++
+		}
+	}
+
+	return count
 }
+
 func Resolve(data []string) [2]any {
 	return [2]any{
 		ResolvePart1(data),
