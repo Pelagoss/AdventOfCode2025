e942ab284c6888f62e8c4bf5efc8bba389e9b465
Cyril HERRERA <cherrera@com-company.fr>
2025-12-03 17:42:43 +0100

    nettoyage

--- Day03/main.go
+++ Day03/main.go
@@ -44,40 +44,6 @@ func ResolveParts(data []string, digitLength int) int {
 	return count
 }
 
-func ResolvePart2(data []string) int {
-	count := 0
-
-	for _, bank := range data {
-		digits := [12]int{}
-
-		for i := 0; i < len(bank); i++ {
-			batterie, _ := strconv.Atoi(string(bank[i]))
-
-			for j := 0; j < len(digits); j++ {
-				if batterie > digits[j] && i < len(bank)+j-1 {
-					for k := j + 1; k < len(digits); k++ {
-						digits[k] = 0
-					}
-					digits[j] = batterie
-					break
-				}
-			}
-		}
-
-		var buffer bytes.Buffer
-
-		for _, digit := range digits {
-			buffer.WriteString(fmt.Sprintf("%v", digit))
-		}
-
-		toAdd, _ := strconv.Atoi(buffer.String())
-
-		count += toAdd
-	}
-
-	return count
-}
-
 func Resolve(data []string) [2]any {
 	return [2]any{
 		ResolveParts(data, 2),
