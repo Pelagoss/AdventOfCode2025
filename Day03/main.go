60f184077e1e70226e44443fe734fa76857e087f
Cyril HERRERA <cherrera@com-company.fr>
2025-12-03 17:42:00 +0100

    Done Day3, Part2

--- Day03/main.go
+++ Day03/main.go
@@ -1,29 +1,42 @@
 package Day03
 
 import (
+	"bytes"
 	"fmt"
 	"strconv"
 )
 
-func ResolvePart1(data []string) int {
+func ResolveParts(data []string, digitLength int) int {
 	count := 0
 
 	for _, bank := range data {
-		higher := 0
-		secondHigher := 0
+		var digits []int
+
+		for i := 0; i < digitLength; i++ {
+			digits = append(digits, 0)
+		}
 
 		for i := 0; i < len(bank); i++ {
 			batterie, _ := strconv.Atoi(string(bank[i]))
 
-			if batterie > higher && i != len(bank)-1 {
-				secondHigher = 0
-				higher = batterie
-			} else if batterie > secondHigher {
-				secondHigher = batterie
+			for j := 0; j < len(digits); j++ {
+				if batterie > digits[j] && i < len(bank)-len(digits)+j+1 {
+					for k := j + 1; k < len(digits); k++ {
+						digits[k] = 0
+					}
+					digits[j] = batterie
+					break
+				}
 			}
 		}
 
-		toAdd, _ := strconv.Atoi(fmt.Sprintf("%v%v", higher, secondHigher))
+		var buffer bytes.Buffer
+
+		for _, digit := range digits {
+			buffer.WriteString(fmt.Sprintf("%v", digit))
+		}
+
+		toAdd, _ := strconv.Atoi(buffer.String())
 
 		count += toAdd
 	}
@@ -32,12 +45,42 @@ func ResolvePart1(data []string) int {
 }
 
 func ResolvePart2(data []string) int {
-	return 0
+	count := 0
+
+	for _, bank := range data {
+		digits := [12]int{}
+
+		for i := 0; i < len(bank); i++ {
+			batterie, _ := strconv.Atoi(string(bank[i]))
+
+			for j := 0; j < len(digits); j++ {
+				if batterie > digits[j] && i < len(bank)+j-1 {
+					for k := j + 1; k < len(digits); k++ {
+						digits[k] = 0
+					}
+					digits[j] = batterie
+					break
+				}
+			}
+		}
+
+		var buffer bytes.Buffer
+
+		for _, digit := range digits {
+			buffer.WriteString(fmt.Sprintf("%v", digit))
+		}
+
+		toAdd, _ := strconv.Atoi(buffer.String())
+
+		count += toAdd
+	}
+
+	return count
 }
 
 func Resolve(data []string) [2]any {
 	return [2]any{
-		ResolvePart1(data),
-		ResolvePart2(data),
+		ResolveParts(data, 2),
+		ResolveParts(data, 12),
 	}
 }
